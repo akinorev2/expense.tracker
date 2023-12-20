0910d30542919ac2bde4962b3c5a252d86725237
akinorev2 <145265946+akinorev2@users.noreply.github.com>
2023-12-20 13:36:31 +0200

    Add files via upload

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+
+    public function run(): void
+    {
+        // \App\Models\User::factory(10)->create();
+
+        \App\Models\User::factory()->create([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+        ]);
+
+        if (\App::environment('local')) {
+            $this->call(DemoSeeder::class);
+        }
+    }
+}
